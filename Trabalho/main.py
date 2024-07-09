b0b2e743934f687072a129ae75dd33f0d11aeeaf
PabloCarnauba <pabloramonsaldanha@gmail.com>
2024-07-09 10:23:30 -0300

    update

--- Trabalho/main.py
+++ Trabalho/main.py
@@ -1,108 +1,52 @@
-import musica
-import player
+import PySimpleGUI as sg
 import os
 import pygame
+from musica import musicas
+from player import Player
+from music_comands import get_files_inside_directory_not_recursive
 
+# Função para atualizar a exibição da música
+def update_song_display(window, musica):
+    window['song_name'].update(musica.nome)
+    window['currently_playing'].update(f'Tocando: {musica.nome} - {musica.artista}')
+
+# Layout da interface gráfica
+layout = [
+    [sg.Text(text='Pressione play..', justification='center', background_color='black', text_color='white', size=(200, 0), font='Tahoma', key='song_name')],
+    [sg.Text('PySimpleGUI Player-Music', background_color='black', text_color='white', font=('Tahoma', 7))],
+    [sg.Image(filename='Images/pylot.png', size=(350, 350))],
+    [sg.Button('Anterior', key='previous'), sg.Button('Play', key='play'), sg.Button('Pausar', key='pause'), sg.Button('Próxima', key='next')],
+    [sg.Text(background_color='black', size=(200, 0), text_color='white', font=('Tahoma', 10), key='currently_playing')]
+]
+
+# Criação da janela
+window = sg.Window('Spotify', layout, size=(480, 730), background_color='black', finalize=True, grab_anywhere=True, resizable=False)
+
+player = Player()
+current_song_index = 0
+
+# Loop principal
 while True:
-    
-    print('''
-SpotNinho - Music App!
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+    elif event == 'play':
+        player.start(musicas[current_song_index])
+        update_song_display(window, musicas[current_song_index])
+    elif event == 'pause':
+        if pygame.mixer.music.get_busy():
+            player.pausar()
+        else:
+            player.despausar()
+    elif event == 'next':
+        if current_song_index + 1 < len(musicas):
+            current_song_index += 1
+            player.start(musicas[current_song_index])
+            update_song_display(window, musicas[current_song_index])
+    elif event == 'previous':
+        if current_song_index > 0:
+            current_song_index -= 1
+            player.start(musicas[current_song_index])
+            update_song_display(window, musicas[current_song_index])
 
-1 - Escutar PlayList completa.
-2 - Listar músicas.
-3 - Informações das músicas.
-4 - Sair.''')
-
-    escolha = int(input("\nInsira uma opção (número): "))
-
-    if escolha == 1:
-        
-        cont = 0
-        
-        while cont < len(musica.musicas):
-            
-            music = musica.musicas[cont]
-            
-            play = player.Player(music)
-            
-            play.start()
-            
-            musica_pausada = False 
-
-            while True:
-                
-                print('''\n1 - Pausar.
-2 - Despausar.
-3 - Próxima.
-4 - Anterior.
-5 - sair da playlist.''')
-                
-                comando = input("\nInsira uma opção: ")
-
-                if comando == '1':
-                    
-                    play.pausar()
-                    
-                    musica_pausada = True
-
-                elif comando == '2' and musica_pausada:
-                    
-                    play.despausar()
-                    
-                    musica_pausada = False
-
-                elif comando == '3':
-                    
-                    play.parar()
-                    
-                    break
-
-                elif comando == '5':
-                    
-                    play.parar()
-                    
-                    cont = len(musica.musicas)  
-                    
-                    break
-                
-                elif comando  == '4' and cont != 0:
-                    
-                    music = musica.musicas[cont - 1]
-                    
-                    play  = player.Player(music)
-                    
-                    play.start()
-                    
-                    cont = cont - 1
-
-                if not musica_pausada and not pygame.mixer.music.get_busy():
-                    
-                    break
-
-            if comando == 'sair':
-                
-                break
-
-            if not musica_pausada:
-                
-                cont += 1
-
-        print("\nVoltando ao menu principal...")
-        
-        os.system("PAUSE")
-        
-        
-
-
-    
-    
-    
-    
-        
-
-     
-
-
-
-
-
+window.close()
--- Trabalho/player.py
+++ Trabalho/player.py
@@ -1,30 +1,22 @@
-import os
 import pygame
-import musica
-import time
+from musica import Musica
 
 pygame.mixer.init()
 
 class Player:
-    
-    def __init__(self, musica):
-        
-        self.musica = musica
+    def __init__(self):
+        self.current_music = None
 
-    def start(self):
-        
-        pygame.mixer.music.load(self.musica.file)
-        
+    def start(self, musica):
+        self.current_music = musica
+        pygame.mixer.music.load(musica.file)
         pygame.mixer.music.play()
 
     def pausar(self):
-        
         pygame.mixer.music.pause()
-        
+
     def despausar(self):
-        
         pygame.mixer.music.unpause()
 
     def parar(self):
-        
         pygame.mixer.music.stop()
